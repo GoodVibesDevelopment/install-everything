4139b89cf85e736acc2a90414012f42653b7ad31
Artur Piersa <good.vibes.development@gmail.com>
2018-01-11 12:16:06 +0100

    Add some soft

--- install-tools-deb.sh
+++ install-tools-deb.sh
@@ -4,6 +4,7 @@
 echo "Add repositories."
 sudo add-apt-repository ppa:ondrej/php
 sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
 
 # update system
 echo "System update"
@@ -20,16 +21,18 @@ sudo apt-get install -y --no-install-recommends \
 	zsh \
 	mitmproxy \
 	tmux \
-	tmuxinator 
+	tmuxinator \
+	fluxgui \
+	rofi \
+	awesome
 
 # jetbrains toolbox
 ./install-jetbrains-toolbox-app.sh
 
 # environment
 git clone https://github.com/GoodVibesDevelopment/dotfiles.git ~/.dotfiles \
-	&& echo ". ~/.dotfiles/.aliases" >> ~/.bashrc
-
-ln -sf  ~/.dotfiles/.gitconfig ~/.gitconfig
+	&& ln -sf ~/.dotfiles/.aliases ~/.bash_aliases \
+	&& ln -sf  ~/.dotfiles/.gitconfig ~/.gitconfig
 
 
 
